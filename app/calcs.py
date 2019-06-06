e535d51398d96b177f5f3d62752f32409b1461f9
juliamedina29 <julia.medina@wework.com>
2019-06-06 11:35:56 -0400

    Flask setup

---
+++ app/calcs.py
@@ -0,0 +1,12 @@
+
+
+def abs_value (delta) :
+    """Returns absolute value of a number"""
+    abs_delta = abs(delta)
+    return(abs_delta)
+
+def offset (delta, miles) :
+    """Returns delta multiplied by miles"""
+    total = delta * miles
+    return total
+
